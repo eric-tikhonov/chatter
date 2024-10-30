1e1ee7925c18166ad4db12ed947d44fc8b163dae
eric-tikhonov <eric.tikhonov@tylertech.com>
2024-10-30 08:55:02 -0500

    ui updates

--- ui/src/App.js
+++ ui/src/App.js
@@ -37,14 +37,18 @@ const App = () => {
           ))}
         </div>
         <div className="input-container">
-          <input
-            type="text"
-            placeholder="Type a message..."
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            onKeyDown={(e) => e.key === "Enter" && sendMessage()}
-          />
-          <button onClick={sendMessage}>Send</button>
+          <div className="message-input">
+            <input
+              type="text"
+              placeholder="Type a message..."
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              onKeyDown={(e) => e.key === "Enter" && sendMessage()}
+            />
+          </div>
+          <div className="send-button">
+            <button onClick={sendMessage}>Send</button>
+          </div>
         </div>
       </div>
     </div>
